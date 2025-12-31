5372d51b10a8199f834cde175c106ce5f8b831f4
Coconut <coconutat@protonmail.com>
2025-12-31 23:42:09 +0800

    Update synckernelsu_all.sh

--- synckernelsu_all.sh
+++ synckernelsu_all.sh
@@ -8,7 +8,7 @@ else
     echo "KernelSU Folder not found, proceeding..."
 fi
 
-read -p "Please enter the version number (A/a is tags version.B/b is main version.C/c is special v0.9.2 Version For Huawei.D/d is SukiSU non-gki.E/e for SukiSU with susfs.): " version
+read -p "Please enter the version number (A/a is tags version.B/b is main version.C/c is special v0.9.2 Version For Huawei.D/d is SukiSU builtin.): " version
 
 if [ "$version" == "" ]; then
     echo "No version specified. Exiting."
@@ -27,10 +27,7 @@ case $version in
         curl -LSs https://raw.githubusercontent.com/Coconutat/KernelSU_backup/0.9.2_upstream_backup/kernel/setup.sh | bash -s 0.9.2_upstream_backup
         ;;
     [Dd]*)
-        curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
-        ;;
-    [Ee]*)
-        curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
+        curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
         ;;
     *)
         echo "Invalid option. Exiting."
