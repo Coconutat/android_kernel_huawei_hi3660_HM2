7b1fee93ab8c4aced9126a72491aacb2a4dc06a1
Coconut <coconutat@protonmail.com>
2026-01-01 12:24:44 +0800

    SukiSU:  manual_hook

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -289,8 +289,8 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-	#ifdef CONFIG_KSU 
-	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	#ifdef CONFIG_KSU
+		ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 	#endif
 
 	/* We only trust the superuser with rebooting the system. */
