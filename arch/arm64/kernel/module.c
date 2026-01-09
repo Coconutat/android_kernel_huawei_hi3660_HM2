00b30614c97a6fae393038347d0c5dd04291fea5
Coconut <coconutat@protonmail.com>
2026-01-09 22:46:58 +0800

    set_memory: fix missed code

--- arch/arm64/kernel/module.c
+++ arch/arm64/kernel/module.c
@@ -26,6 +26,7 @@
 #include <linux/mm.h>
 #include <linux/moduleloader.h>
 #include <linux/vmalloc.h>
+#include <linux/set_memory.h>
 #include <asm/alternative.h>
 #include <asm/insn.h>
 #include <asm/sections.h>
