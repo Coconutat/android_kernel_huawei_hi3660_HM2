f3fd99552352382b9fb0cf2c3d6efa09cf36fff7
Coconut <coconutat@protonmail.com>
2026-01-18 18:19:36 +0800

    Restore hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2291,13 +2291,15 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
 {
+	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
+	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
+	int rc;
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
-
 
 	/*
 	 * The only transitions we permit under NNP or nosuid
