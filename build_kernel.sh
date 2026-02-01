3a4d4916a906bd8dc12f3b22ea802a8fcaaad413
Coconut <coconutat@protonmail.com>
2026-02-01 17:05:04 +0800

    Script: Add KPM Patch

--- build_kernel.sh
+++ build_kernel.sh
@@ -48,10 +48,6 @@ if [ -f out/arch/arm64/boot/Image.gz ]; then
     mkdir -p Build_Kernel/Original
     mkdir -p Build_Kernel/KPM
 
-	# 定义 mkbootimg 的通用参数 (方便后续调用)
-	BOOT_ARGS="--base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-10-01"
-	BOOT_ARGS2="--base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-10-01"
-
     # =========================================================
     # PART 1: 处理原始未修补内核 (Original)
     # =========================================================
@@ -61,8 +57,8 @@ if [ -f out/arch/arm64/boot/Image.gz ]; then
     cp out/arch/arm64/boot/Image.gz Build_Kernel/Original/Image.gz
     
     # 2. 打包原始 img
-    ./tools/mkbootimg --kernel Build_Kernel/Original/Image.gz $BOOT_ARGS --output Build_Kernel/Original/Kirin960_HM2_Kernel.img
-	./tools/mkbootimg --kernel Build_Kernel/Original/Image.gz $BOOT_ARGS2 --output Build_Kernel/Original/Kirin960_HM2_Kernel_PM.img
+    ./tools/mkbootimg --kernel Build_Kernel/Original/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-10-01 --output Build_Kernel/Original/Kirin960_HM2_Kernel.img
+	./tools/mkbootimg --kernel Build_Kernel/Original/Image.gz -base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-10-01 --output Build_Kernel/Original/Kirin960_HM2_Kernel_PM.img
     
     echo "原始内核已保存至: Build_Kernel/Original/"
 
@@ -79,34 +75,34 @@ if [ -f out/arch/arm64/boot/Image.gz ]; then
         
         if [ -f "$PATCH_TOOL" ]; then
             # 赋予工具执行权限
-            chmod +x "$PATCH_TOOL"
+            chmod 777 "$PATCH_TOOL"
 
             echo "准备工作环境..."
             # 将原始 Image.gz 复制到当前目录准备解压，避免污染 Original 目录
-            cp Build_Kernel/Original/Image.gz Image.gz.temp
-            
-            echo "正在解压 Image.gz ..."
-            gzip -d -f -S .temp Image.gz.temp
-            # 解压后文件名应为 Image.gz (因为后缀是.temp) -> Image
-            mv Image.gz Image
+            cp out/arch/arm64/boot/Image Build_Kernel/KPM/Image
+            cd Build_Kernel/KPM/
 
             echo "正在运行 patch_linux 工具..."
-            $PATCH_TOOL
+            ../../$PATCH_TOOL
 
             # 检查是否生成了 oImage (KernelSU/APatch 常见行为)
             if [ -f "oImage" ]; then
                 echo "检测到 oImage，替换原 Image..."
-                mv oImage Image
+                mv -f oImage Image
             fi
+
+            ls -l
 
             echo "正在重新压缩为 Image.gz 并保存到 KPM 目录..."
             # 压缩并输出到 KPM 目录
-            gzip -c Image > Build_Kernel/KPM/Image.gz
+            gzip -k Image Image.gz
+            ls -l
+            cd ../..
             
             echo "正在打包修补后的内核 (KPM)..."
             # 使用修补后的内核进行打包
-            ./tools/mkbootimg --kernel Build_Kernel/KPM/Image.gz $BOOT_ARGS --output Build_Kernel/KPM/Kirin960_HM2_Kernel_KPM.img
-			./tools/mkbootimg --kernel Build_Kernel/KPM/Image.gz $BOOT_ARGS2 --output Build_Kernel/KPM/Kirin960_HM2_Kernel_KPM_PM.img
+            ./tools/mkbootimg --kernel Build_Kernel/Original/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-10-01 --output Build_Kernel/KPM/Kirin960_HM2_Kernel_KPM.img
+	        ./tools/mkbootimg --kernel Build_Kernel/Original/Image.gz -base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-10-01 --output Build_Kernel/KPM/Kirin960_HM2_Kernel_KPM_PM.img
 
             # 清理临时文件
             rm -f Image
