d7b535a1e0e6efd9c64d829dfe80703c94c20211
Coconut <coconutat@protonmail.com>
2026-02-01 14:24:39 +0800

    SUSFS: Fix susfs compile

--- fs/readdir.c
+++ fs/readdir.c
@@ -232,95 +232,81 @@ struct getdents_callback {
 static int filldir(struct dir_context *ctx, const char *name, int namlen,
 		   loff_t offset, u64 ino, unsigned int d_type)
 {
-	struct linux_dirent __user * dirent;
-	struct getdents_callback *buf =
-		container_of(ctx, struct getdents_callback, ctx);
-	unsigned long d_ino;
-	int reclen = ALIGN(offsetof(struct linux_dirent, d_name) + namlen + 2,
-		sizeof(long));
+    struct linux_dirent __user * dirent;
+    struct getdents_callback *buf =
+        container_of(ctx, struct getdents_callback, ctx);
+    unsigned long d_ino;
+    int reclen = ALIGN(offsetof(struct linux_dirent, d_name) + namlen + 2,
+        sizeof(long));
 
-	#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	struct inode *inode;
-	#endif
+    #ifdef CONFIG_KSU_SUSFS_SUS_PATH
+    struct inode *inode;
+    #endif
+        
+    buf->error = -EINVAL;   /* only used if we fail.. */
+    if (reclen > buf->count)
+        return -EINVAL;
+    d_ino = ino;
+    if (sizeof(d_ino) < sizeof(ino) && d_ino != ino) {
+        buf->error = -EOVERFLOW;
+        return -EOVERFLOW;
+    }
+
+    /* === [保留] 正确的 SUSFS 逻辑位置 (在处理 previous 之前) === */
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+    if (buf->is_base_dentry_android_data_root_dir) {
+        if (susfs_is_sus_android_data_d_name_found(name)) {
+            return 0;
+        }
+    } else if (buf->is_base_dentry_sdcard_root_dir) {
+        if (susfs_is_sus_sdcard_d_name_found(name)) {
+            return 0;
+        }
+    }
+
+    inode = ilookup(buf->sb, ino);
+    if (!inode) {
+        goto orig_flow;
+    }
+    if (susfs_is_inode_sus_path(inode)) {
+        iput(inode);
+        return 0;
+    }
+    iput(inode);
+orig_flow:
+#endif
+    /* === [结束] === */
+
+    dirent = buf->previous;
+    if (dirent) {
+        if (signal_pending(current))
+            return -EINTR;       
 		
-	buf->error = -EINVAL;	/* only used if we fail.. */
-	if (reclen > buf->count)
-		return -EINVAL;
-	d_ino = ino;
-	if (sizeof(d_ino) < sizeof(ino) && d_ino != ino) {
-		buf->error = -EOVERFLOW;
-		return -EOVERFLOW;
-	}
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (buf->is_base_dentry_android_data_root_dir) {
-		if (susfs_is_sus_android_data_d_name_found(name)) {
-			return 0;
-		}
-	} else if (buf->is_base_dentry_sdcard_root_dir) {
-		if (susfs_is_sus_sdcard_d_name_found(name)) {
-			return 0;
-		}
-	}
+        if (__put_user(offset, &dirent->d_off))
+            goto efault;
+    }
 
-	inode = ilookup(buf->sb, ino);
-	if (!inode) {
-		goto orig_flow;
-	}
-	if (susfs_is_inode_sus_path(inode)) {
-		iput(inode);
-		return 0;
-	}
-	iput(inode);
-orig_flow:
-#endif
-	dirent = buf->previous;
-	if (dirent) {
-		if (signal_pending(current))
-			return -EINTR;
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (buf->is_base_dentry_android_data_root_dir) {
-		if (susfs_is_sus_android_data_d_name_found(name)) {
-			return 0;
-		}
-	} else if (buf->is_base_dentry_sdcard_root_dir) {
-		if (susfs_is_sus_sdcard_d_name_found(name)) {
-			return 0;
-		}
-	}
+    /* 更新 previous 指针，准备处理下一个 */
+    buf->previous = (struct linux_dirent __user *)((char *)buf->current_dir + reclen);
+    dirent = buf->current_dir;
+    
+    /* 下面是写入 dirent 的常规操作 */
+    if (__put_user(d_ino, &dirent->d_ino) ||
+        __put_user(reclen, &dirent->d_reclen) ||
+        __put_user(d_type, (char __user *) dirent + reclen - 1) ||
+        copy_to_user(dirent->d_name, name, namlen) ||
+        __put_user(0, dirent->d_name + namlen))
+        goto efault;
+    
+    buf->previous = dirent;
+    dirent = (void __user *)dirent + reclen;
+    buf->current_dir = dirent;
+    buf->count -= reclen;
+    return 0;
 
-	inode = ilookup(buf->sb, ino);
-	if (!inode) {
-		goto orig_flow;
-	}
-	if (susfs_is_inode_sus_path(inode)) {
-		iput(inode);
-		return 0;
-	}
-	iput(inode);
-orig_flow:
-#endif
-		if (__put_user(offset, &dirent->d_off))
-			goto efault;
-	}
-	dirent = buf->current_dir;
-	if (__put_user(d_ino, &dirent->d_ino))
-		goto efault;
-	if (__put_user(reclen, &dirent->d_reclen))
-		goto efault;
-	if (copy_to_user(dirent->d_name, name, namlen))
-		goto efault;
-	if (__put_user(0, dirent->d_name + namlen))
-		goto efault;
-	if (__put_user(d_type, (char __user *) dirent + reclen - 1))
-		goto efault;
-	buf->previous = dirent;
-	dirent = (void __user *)dirent + reclen;
-	buf->current_dir = dirent;
-	buf->count -= reclen;
-	return 0;
 efault:
-	buf->error = -EFAULT;
-	return -EFAULT;
+    buf->error = -EFAULT;
+    return -EFAULT;
 }
 
 SYSCALL_DEFINE3(getdents, unsigned int, fd,
